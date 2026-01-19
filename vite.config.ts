2a418646f58542c36f4d378f71caba92e0bf086e
03-renate <renate.pedersen03@gmail.com>
2026-01-19 15:54:43 +0100

    feat: add Netlify redirects configuration and update Vite config for proper directory resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
+  }
 })
